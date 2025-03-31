b92f77203bbdcec96e15744ffb6a5cf23c64c782
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2025-03-31 17:35:25 -0500

    clases agregadas sin implementar

---
+++ proyectoPokemones/src/models/Pokemon.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author juand
+ * @since 20250331
+ * @version 1.0
+ */
+public abstract class Pokemon {
+    
+}
