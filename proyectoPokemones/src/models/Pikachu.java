2e5a8197688115ace7ebf1005d871f62937b0390
unknown <evers.uribeq@autonoma.edu.co>
2025-04-01 14:05:18 -0500

    implementacion pikachu y pokemon electrico

--- proyectoPokemones/src/models/Pikachu.java
+++ proyectoPokemones/src/models/Pikachu.java
@@ -1,48 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
 /**
- * Clase que representa a Pikachu, un Pokémon de tipo eléctrico.
+ * Clase que representa al Pokémon Pikachu, un Pokémon de tipo eléctrico.
  * Extiende la clase abstracta Pokemon e implementa la interfaz PokemonElectrico.
  * 
  * @author juand
- * @since 20250331
+ * @since 2025-03-31
  * @version 1.0
  */
 public class Pikachu extends Pokemon implements PokemonElectrico {
-
+    
     /**
      * Constructor de la clase Pikachu.
      * 
-     * @param numPokedex Número de la Pokédex del Pokémon.
-     * @param peso Peso del Pokémon en kilogramos.
-     * @param nombre Nombre del Pokémon.
-     * @param temporada Temporada en la que aparece el Pokémon.
+     * @param numPokedex Número de la Pokédex de Pikachu.
+     * @param peso Peso de Pikachu en kilogramos.
+     * @param nombre Nombre de Pikachu.
+     * @param temporada Temporada en la que aparece Pikachu.
      */
     public Pikachu(String numPokedex, float peso, String nombre, int temporada) {
         super(numPokedex, peso, nombre, temporada);
     }
-
+    
+    /**
+     * Realiza el ataque Impactrueno, específico de Pokémon tipo eléctrico.
+     * 
+     * @return true si el ataque se ejecuta correctamente.
+     */
     @Override
-    public void atacarImpactrueno() {
+    public boolean atacarImpactrueno() {
         System.out.println(getNombre() + " usó Impactrueno!");
+        return true;
     }
-
+    
+    /**
+     * Realiza el ataque Puño Trueno, específico de Pokémon tipo eléctrico.
+     * 
+     * @return true si el ataque se ejecuta correctamente.
+     */
     @Override
-    public void atacarPuniotrueno() {
+    public boolean atacarPunoTrueno() {
         System.out.println(getNombre() + " usó Puño Trueno!");
+        return true;
     }
-
+    
+    /**
+     * Realiza el ataque Rayo, específico de Pokémon tipo eléctrico.
+     * 
+     * @return true si el ataque se ejecuta correctamente.
+     */
     @Override
-    public void atacarRayo() {
+    public boolean atacarRayo() {
         System.out.println(getNombre() + " usó Rayo!");
+        return true;
     }
-
+    
+    /**
+     * Realiza el ataque Rayo Carga, específico de Pokémon tipo eléctrico.
+     * 
+     * @return true si el ataque se ejecuta correctamente.
+     */
     @Override
-    public void atacarRayoCarga() {
+    public boolean atacarRayoCarga() {
         System.out.println(getNombre() + " usó Rayo Carga!");
+        return true;
     }
 }
